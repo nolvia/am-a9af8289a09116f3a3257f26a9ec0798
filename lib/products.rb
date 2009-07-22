42f62b7e56a03330d4c0358d61416816ad6f340a
Elia Schito <perlelia@gmail.com>
2009-07-22 18:59:36 +0200

    Updated products translations and added youtube videos support.

--- lib/products.rb
+++ lib/products.rb
@@ -8,12 +8,27 @@ require 'YAML'
 files = Dir[File.dirname(__FILE__)+'/products.*.txt']
 locale = nil
 
+
+videos = {}
+YAML.load_file(File.dirname(__FILE__)+'/product_videos.yml').each do |video|
+  category_id = video['category'].to_i
+  product_id  = video['product'].to_i
+  videos[category_id] ||= {}
+  videos[category_id][product_id] ||= []
+  videos[category_id][product_id] << video
+  puts "video: #{video['title']} (#{category_id.inspect}:#{product_id.inspect})"
+end
+
+pp videos
+
 files.each do |file|
   puts
   puts '*' * 80
   puts
 
-  category, categories = '', {}
+  category, categories, categories_list = '', {}, []
+  category_index = 0
+  product_index  = 0
   
   
   locale = file.scan(/products\.(.*?)\.txt/).flatten.first
@@ -30,23 +45,31 @@ files.each do |file|
   content.each do |c|
     if (/^\#/ =~ c)
       puts
+      category_index += 1
+      product_index   = 0
       category = c.downcase
       category.gsub!(/\W/, '')
       puts "Found category: #{category.inspect}"
-      
+      categories_list << category
+      puts "CATEGORY: #{category} (#{category_index.inspect})"
       categories[category] = []
     else
       products = c.scan(/\d+\n§.*?\n.*?\n/)
       products.map! do |product|
-        # puts '> '+product.inspect
+        product_index += 1
+        puts "> #{product_index.inspect}"
         product_title, product_description = product.scan(/§(.*?)\n(.*?)\n/).first
         product_title = product_title.strip.downcase
         product_title.gsub!(/(\b)(\w)/) do |letter|
           letter.upcase
         end
-        {'title' => product_title, 'description' => product_description}
+        hash = {'title' => product_title, 'description' => product_description}
+        if videos[category_index] and videos[category_index][product_index]
+          puts "VIDEO: #{videos[category_index][product_index].inspect}"
+          hash['video'] = videos[category_index][product_index]
+        end
+        hash
       end
-      print '.' * products.size
       categories[category] = products
     end
   end
@@ -54,7 +77,10 @@ files.each do |file|
   
   yml_file_name = File.dirname(__FILE__)+"/products.#{locale}.yml"
   File.open(yml_file_name, 'w') do |yml_file|
-    yml_file << categories.to_yaml
+    yml_file << {
+      'per_category' => categories, 
+      'categories_list' => categories_list
+    }.to_yaml
   end
   
   puts "Wrote: #{yml_file_name}" if File.exist? yml_file_name
