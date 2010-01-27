0d86353affcfabf3d417740745b83cb85c77809c
Elia Schito <perlelia@gmail.com>
2010-01-27 18:16:31 +0100

    Extracted picture texts and adde translation support.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,64 +1,8 @@
 class Picture
-  attr_reader :id, :path
+  include PictureTranslations
   
-  DATA = {
-      :orchestra_polydor_parigi => {
-        :description => 
-          "Negli studi di registrazione della Polydor di Parigi Aldo Maietti
-          dirige la sua orchestra di 18 elementi avvalendosi 
-          di valenti musicisti di diverse nazionalità con il celebre 
-          cantante argentino Ernesto Rondò.",
-        :place => "Studi Polydor, Parigi",
-        :date => "1959",
-        :position => 9
-      },
-      :maietti_dirige => {
-        :description => 
-          "Presso il Teatro Astral di Buenos Aires, gremito di pubblico, 
-          al 25˚ anniversario della scomparsa del più celebre cantante 
-          argentino di Tanghi: Carlos Gardel; il maestro Francisco Canaro, 
-          notissimo compositore di tanghi e direttore d'orchestra, cede la 
-          direzione della sua orchestra al maestro Maietti in segno di stima,
-          ammirazione ed ospitalità.
-          Il Maestro Maietti accompagna il cantante argentino Ernesto Rondò 
-          nel tango «Mano a Mano» dello stesso Gardel, sotto scroscianti 
-          applausi.", 
-        :place => "Teatro Astral, Buenos Aires",
-        :date => "24 Giugno 1961",
-        :position => 10
-      },
-      
-      :canaro_invita_maietti => {
-        # :description => 
-        #   "",
-        :date => "24 Giugno 1961",
-        :place => "Buenos Aires",
-        :position => 13
-      },
-      
-      :rondo_maietti_canaro => {
-        :description => 
-          "Nella foto: Ernesto Rondò (cantante argentino), Aldo Maietti e 
-          Francisco Canaro che è stato il più noto compositore di tanghi e 
-          direttore d'orchestra",
-        :date => "24 Giugno 1961",
-        :place => "Buenos Aires",
-        :position => 13
-      },
-      
-      :radio_elmundo => {
-        :description => 
-          "A radio «El Mundo», da sinistra: Aldo Maietti, Edmundo Rivero 
-          (il più celebre dei cantanti di tanghi argentini degli anni '60),
-          Govi (violinista e direttore d'orchestra), lo speaker ed il 
-          segretario della radio.",
-        :date => "1964",
-        :place => "Buenos Aires",
-        :position => 15
-        
-      } 
-    
-  }
+  attr_reader :id, :path
+  attr_accessor :locale
   
   def self.all
     Dir[Rails.public_path / 'images' / 'pictures' / '*'].map do |path|
@@ -71,7 +15,7 @@ class Picture
   end
   
   def data
-    DATA[path.basename(".*").to_s.to_sym] || {}
+    @data ||= DATA[locale || :it][path.basename(".*").to_s.to_sym] || {}
   end
   
   def title;        data[:title      ] end
@@ -81,7 +25,4 @@ class Picture
   def position;     data[:position   ] || 1000            end
   def description;  data[:description].try :gsub,/\s+/," " end
   
-  
-  
 end
-
---
+++ app/models/picture_translations.rb
@@ -0,0 +1,70 @@
+module PictureTranslations
+  DATA = {
+    :it => {
+        :orchestra_polydor_parigi => {
+          :description => 
+            "Negli studi di registrazione della Polydor di Parigi Aldo Maietti
+            dirige la sua orchestra di 18 elementi avvalendosi 
+            di valenti musicisti di diverse nazionalità con il celebre 
+            cantante argentino Ernesto Rondò.",
+          :place => "Studi Polydor, Parigi",
+          :date => "1959",
+          :position => 9
+        },
+
+        :maietti_dirige => {
+          :description => 
+            "Presso il Teatro Astral di Buenos Aires, gremito di pubblico, 
+            al 25˚ anniversario della scomparsa del più celebre cantante 
+            argentino di Tanghi: Carlos Gardel; il maestro Francisco Canaro, 
+            notissimo compositore di tanghi e direttore d'orchestra, cede la 
+            direzione della sua orchestra al maestro Maietti in segno di stima,
+            ammirazione ed ospitalità.
+            Il Maestro Maietti accompagna il cantante argentino Ernesto Rondò 
+            nel tango «Mano a Mano» dello stesso Gardel, sotto scroscianti 
+            applausi.", 
+          :place => "Teatro Astral, Buenos Aires",
+          :date => "24 Giugno 1961",
+          :position => 10
+        },
+
+        :canaro_invita_maietti => {
+          :description => 
+            "Al \"Teatro Astral\" di Buenos Aires, il Maestro Francisco Canaro 
+            invita il Maestro Aldo Maietti a dirigere la sua orchestra 
+            accompagnando il cantnte Ernesto Rondò nel tango \"Mano a Mano\" di 
+            Carlos Gardel (il più grando cantante argentino di tanghi negli anni 
+            30). Commemorazione in onore al XXV anniversario della scomparsa di 
+            Carlos Gardel.
+            Sullo sfondo la foto di Carlos Gardel, perito in un incidente aereo 
+            il 24 Giugno del 1935 Medellin.",
+          :date => "24 e 25 Giugno 1961",
+          :place => "Buenos Aires",
+          :position => 13
+        },
+
+        :rondo_maietti_canaro => {
+          :description => 
+            "Nella foto: Ernesto Rondò (cantante argentino), Aldo Maietti e 
+            Francisco Canaro che è stato il più noto compositore di tanghi e 
+            direttore d'orchestra",
+          :date => "24 Giugno 1961",
+          :place => "Buenos Aires",
+          :position => 13
+        },
+
+        :radio_elmundo => {
+          :description => 
+            "A radio «El Mundo», da sinistra: Aldo Maietti, Edmundo Rivero 
+            (il più celebre dei cantanti di tanghi argentini degli anni '60),
+            Govi (violinista e direttore d'orchestra), lo speaker ed il 
+            segretario della radio.",
+          :date => "1964",
+          :place => "Buenos Aires",
+          :position => 15
+
+        } 
+
+    }
+  
+end
